07f491f3c8ff73ba5fd662c8a39e818d589f5259
Burak GÜL <developerburakgul@gmail.com>
2025-06-24 09:30:04 +0300

    fix: error from python 3.10

--- Models/Message.py
+++ Models/Message.py
@@ -1,11 +1,12 @@
 from datetime import datetime
+from typing import Optional
 from pydantic import BaseModel, field_serializer
 
 class Message(BaseModel):
     timestamp: datetime
     sender: str
     text: str
-    translated_text: str | None = None  # <-- EKLENDİ
+    translated_text: Optional[str] = None  # <-- DÜZELTİLDİ
 
     @field_serializer('timestamp')
     def serialize_datetime(self, dt: datetime, _info):
